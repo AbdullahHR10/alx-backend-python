d09697e2549c5c155ea8f6cb4eace9b3a0c31a5f
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-10-10 10:50:03 +0300

    Add floor function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Module that contains the floor function. """
+
+import math
+
+
+def floor(n: float) -> float:
+    """ Type-annotated function that takes a float and returns the floor. """
+    return math.floor(n)
