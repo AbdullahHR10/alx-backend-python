61276b7acb4fd067f43a6650e9f2c111c4fed4c7
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-10-10 16:40:24 +0300

    Change the apporach

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,7 +5,4 @@
 def sum_list(input_list: list[float]) -> float:
     """ Type-annotated function that takes a list of floats
     and returns their sum as a float."""
-    sum: float = 0.0
-    for number in input_list:
-        sum += number
-    return sum
+    return sum(input_list)
