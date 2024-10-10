d8839eb1183fe566f10c38e540e8d70b3ee1a86f
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-10-10 18:47:20 +0300

    Fix function

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """ Module that contains the safely_get_value function. """
 
-from typing import Mapping, Any, TypeVar, Union
+from typing import Mapping, Any, Union, TypeVar
+T = TypeVar('T')
 
-T = TypeVar('T')  # Declare a type variable
 
-
-def safely_get_value(dct: Mapping[Any, T], key: Any, default: Union[T, None] = None) -> Union[T, None]:
-    """Function that safely retrieves a value from a dictionary."""
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
+    """ Function that safely retrieves a value from a dictionary. """
     if key in dct:
         return dct[key]
     else:
