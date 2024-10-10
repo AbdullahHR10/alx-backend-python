587756df84893a34875193d1ac2315f79429e3f3
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-10-10 09:36:07 +0300

    Add add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Module that contains the add function. """
+
+
+def add(a: float, b: float) -> float:
+    """ Type-annotated function that returns the sum of a and b. """
+    return a + b
