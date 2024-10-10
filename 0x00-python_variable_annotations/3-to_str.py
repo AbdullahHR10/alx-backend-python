cb6b66f9802b9cd59ae96260819284cbc9b793a7
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-10-10 10:51:40 +0300

    Add to_str function

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Module that contains the to_str function. """
+
+
+def to_str(n: float) -> str:
+    """ Type-annotated function that returns string representation of n. """
+    return str(n)
