ec2c9a67251ad89faa11861d7e633327a28b494e
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-11-11 08:19:13 +0200

    Add User model that extends from the Abstract user

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,21 @@
+"""Django app models."""
+import uuid
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """User model that extends from the Abstract user."""
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True, null=False)
+    password_hash = models.CharField(max_length=50, null=False)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    ROLE_CHOICES = [
+        ('guest', 'Guest'),
+        ('host', 'Host'),
+        ('admin', 'Admin'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='guest', null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
